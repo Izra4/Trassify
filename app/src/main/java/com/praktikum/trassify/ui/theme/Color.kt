2e4627a761212b3f0bc934121c8a5bf50f6f629d
Gamachi <maulanaufil01@gmail.com>
2024-12-11 01:18:10 +0700

    Pesan commit Anda

--- app/src/main/java/com/praktikum/trassify/ui/theme/Color.kt
+++ app/src/main/java/com/praktikum/trassify/ui/theme/Color.kt
@@ -12,6 +12,8 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val White = Color(0xFFFFFFFF)
 val Gray10 = Color(0xFFB6B4C2)
 val Gray20 = Color(0XFF9F9F9F)
 val Primary10 = Color(0xFF7C66FF)
+
