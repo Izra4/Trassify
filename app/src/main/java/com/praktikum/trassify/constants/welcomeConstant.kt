445dfd12ef79693cd34f0795f21a1d0a96974648
izra4 <aryaizra2@gmail.com>
2024-12-11 20:07:54 +0700

    refactor: refactoring the worst ever structur that i've ever seen

--- app/src/main/java/com/praktikum/trassify/constants/welcomeConstant.kt
+++ app/src/main/java/com/praktikum/trassify/constants/welcomeConstant.kt
@@ -1,27 +1,32 @@
 package com.praktikum.trassify.constants
 
 import com.praktikum.trassify.R
-import com.praktikum.trassify.data.model.WelcomePage
 
-val welcomeContent : List<WelcomePage> = listOf(
-    WelcomePage(
+val welcomeContent : List<WelcomeRepository> = listOf(
+    WelcomeRepository(
         image = R.drawable.welcome1,
         title = "Selamat Datang di Trassify",
         description = "Jaga kebersihan lingkungan kini lebih mudah! Dengan Trassify, Kamu dapat membantu menjaga kota tetap bersih, melaporkan sampah, dan mendapatkan reward dari aksi peduli lingkungan Kamu!"
     ),
-    WelcomePage(
+    WelcomeRepository(
         image = R.drawable.welcome2,
         title = "Jadwal Pemungutan Sampah",
         description = "Dapatkan notifikasi tentang jadwal pemungutan sampah di daerah Kamu. Tidak ada lagi kekhawatiran tentang sampah yang menumpuk!"
     ),
-    WelcomePage(
+    WelcomeRepository(
         image = R.drawable.welcome3,
         title = "Lokasi Sampah di Sekitarmu",
         description = "Lihat sampah? Foto, laporkan, dan biarkan kami yang menindaklanjuti! Aplikasi ini akan secara otomatis mencatat lokasi sampah dan mengirim laporan ke petugas terkait."
     ),
-    WelcomePage(
+    WelcomeRepository(
         image = R.drawable.welcome4,
         title = "Kumpulkan Poin dari Aksimu",
         description = "Setiap sampah yang berhasil Kamu laporkan dan diangkut oleh petugas akan memberimu poin! Tukarkan poin ini dengan reward menarik sebagai apresiasi atas kontribusi Kamu terhadap kebersihan lingkungan."
     )
+)
+
+data class WelcomeRepository(
+    val image : Int,
+    val title : String,
+    val description : String
 )
